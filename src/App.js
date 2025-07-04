b85da6d88dad1122489e2f0ac0ed8883a489c500
Juan Torres <juan93git@gmail.com>
2025-07-04 07:49:23 +0000

    Added sass skeleton for styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>TODO: create app</div>;
+  return (
+    <div>
+      TODO: create app
+      {/* NavBar */}
+      {/* Main */}
+      {/* == Box List */}
+      {/* ==== Exercise Item */}
+      {/* == Box Content */}
+      {/* ==== Stats */}
+      {/* ==== (OR) Exercise Details */}
+      {/* ==== (OR) Fav Exercises List */}
+      {/* ====== Exercise Item */}
+    </div>
+  );
 }
 
 export default App;
